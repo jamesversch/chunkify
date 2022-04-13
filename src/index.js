65cab63290073eec69dcad0b5eca9abb61ab843e
jamesversch17 <james.r.verschleiser.22@dartmouth.edu>
2022-04-13 17:15:21 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+
+function increment() {
+  num += 1;
+  $('#main').html(`You've been on this page for ${num} seconds.`);
+}
+
+setInterval(increment, 1000);
